aa92c06a5f672584f969950d5c0a5ff34f57c48d
Edgar Conrado <edgar.conrado@oreillyauto.mx>
2026-04-21 23:30:08 -0600

    fix: add EAS projectId to app.config.js

--- app.config.js
+++ app.config.js
@@ -3,19 +3,19 @@
 // Expone las variables de entorno EXPO_PUBLIC_* al bundle de JS.
 export default {
   expo: {
-    name: 'Paletería App',
-    slug: 'paleteria-app',
+    name: 'GelatoFlow App',
+    slug: 'gelato-flow-app',
     version: '1.0.0',
-    scheme: 'paleteria',
+    scheme: 'gelato-flow',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     newArchEnabled: true,
     ios: {
       supportsTablet: false,
-      bundleIdentifier: 'mx.paleteria.app',
+      bundleIdentifier: 'mx.gelato-flow.app',
     },
     android: {
-      package: 'mx.paleteria.app',
+      package: 'mx.gelato-flow.app',
       edgeToEdgeEnabled: true,
     },
     web: {
@@ -35,7 +35,7 @@ export default {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       eas: {
-        projectId: process.env.EAS_PROJECT_ID,
+        projectId: 'caf53e81-0efe-4d3d-9e0d-99cb5e9aead3',
       },
     },
   },
