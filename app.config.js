bf9c19d53bc5d1b7f10af67f478e8a2d8185c725
Edgar Conrado <edgar.conrado@oreillyauto.mx>
2026-04-23 11:06:18 -0600

    fix: update EAS projectId to 34e3d317

--- app.config.js
+++ app.config.js
@@ -46,7 +46,7 @@ export default {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       eas: {
-        projectId: 'caf53e81-0efe-4d3d-9e0d-99cb5e9aead3',
+        projectId: '34e3d317-1a87-43a0-a9ef-1c4ec47d36d5',
       },
     },
   },
